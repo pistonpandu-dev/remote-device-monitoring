71f7f5acb1fce492d308b9b5a26e07bd8481be12
pistonpandu-dev <pistonpandu@gmail.com>
2026-07-03 21:32:46 +0700

    Create register-sw.ts

---
+++ src/lib/pwa/register-sw.ts
@@ -0,0 +1,26 @@
+export function registerServiceWorker() {
+  if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('SW registered:', registration);
+        })
+        .catch((error) => {
+          console.log('SW registration failed:', error);
+        });
+    });
+  }
+}
+
+export function unregisterServiceWorker() {
+  if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then((registration) => {
+        registration.unregister();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+}
